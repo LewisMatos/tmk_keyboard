592120b4134ade0ad0493008cc0b4b1d89cfb18a
Pierre Carrier <pcarrier@twitter.com>
2014-09-16 23:08:20 -0700

    working on 3 layers

--- keyboard/gh60/keymap_planck.c
+++ keyboard/gh60/keymap_planck.c
@@ -1,43 +1,48 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    KEYMAP( /* 0: QWERTY */
+    [0] = KEYMAP( /* Native */
       Q,    W,    E,    R,    T,    QUOT, BSLS, Y,    U,    I,    O,   P,
       A,    S,    D,    F,    G,    ESC,  TAB,  H,    J,    K,    L,   SCLN,
       Z,    X,    C,    V,    B,    BSPC, ENT,  N,    M,    COMM, DOT, SLSH,
       LCTL, LALT, LGUI, LSFT, FN1,  SPC,        FN2,  LEFT, DOWN, UP,  RGHT),
-    KEYMAP( /* 1: fn1 */
-      FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, FN20, FN21,
-      1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,
-      NO,   NO,   GRV,  FN22, NO,   TRNS, TRNS, NO,   LBRC, RBRC, FN23, FN24,
-      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, HOME, PGDN, PGUP, END),
-    KEYMAP( /* 2: fn2 */
-      F13,  F14,  F15,  F16,  F17,  F18,  F19,  F20,  F21,  F22,  F23,  F24,
-      F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,
-      FN3,  FN4,  FN5,  TRNS, DEL,  TRNS, TRNS, CAPS, INS,  PSCR, SLCK, PAUS,
-      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, MUTE, VOLD, VOLU, MPLY),
-    KEYMAP( /* 3: fn1+fn2 */
-      FN3,  FN4,  FN5,  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   PWR,
-      NO,   NO,   NO,   UNDO, NO,   NO,   NO,   NO,   AGAIN, NO,  NO,   SLEP,
-      NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   WAKE,
-      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, MSTP, MPRV, MNXT, MPLY),
-    KEYMAP( /* 4: Colemak */
+    [1] = KEYMAP( /* QWERTY->Colemak */
       Q,    W,    F,    P,    G,    QUOT, BSLS, J,    L,    U,    Y,   SCLN,
       A,    R,    S,    T,    D,    ESC,  TAB,  H,    N,    E,    I,   O,
       Z,    X,    C,    V,    B,    BSPC, ENT,  K,    M,    COMM, DOT, SLSH,
       LCTL, LALT, LGUI, LSFT, FN1,  SPC,        FN2,  LEFT, DOWN, UP,  RGHT),
-    KEYMAP( /* 5: Dvorak */
+    [2] = KEYMAP( /* 2: QWERTY->Dvorak */
       QUOT, COMM, DOT,  P,    Y,    SLSH, BSLS, F,    G,    C,    R,   L,
       A,    O,    E,    U,    I,    ESC,  TAB,  D,    H,    T,    N,   S,
       SCLN, Q,    J,    K,    X,    BSPC, ENT,  B,    M,    W,    V,   Z,
       LCTL, LALT, LGUI, LSFT, FN1,  SPC,        FN2,  LEFT, DOWN, UP,  RGHT),
+    [4] = KEYMAP( /* fn1 */
+      FN10, FN11, FN12, FN13, FN14, FN15, FN16, FN17, FN18, FN19, FN20, FN21,
+      1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,
+      NO,   NO,   GRV,  FN22, NO,   TRNS, TRNS, NO,   LBRC, RBRC, FN23, FN24,
+      TRNS, TRNS, TRNS, TRNS, FN1,  TRNS,       FN3,  HOME, PGDN, PGUP, END),
+    [5] = KEYMAP( /* fn2 */
+      F13,  F14,  F15,  F16,  F17,  F18,  F19,  F20,  F21,  F22,  F23,  F24,
+      F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,
+      NO,   NO,   NO,   NO,   DEL,  TRNS, TRNS, CAPS, INS,  PSCR, SLCK, PAUS,
+      TRNS, TRNS, TRNS, TRNS, FN3,  TRNS,       FN2,  MUTE, VOLD, VOLU, MPLY),
+    [6] = KEYMAP( /* fn1+fn2 */
+      FN7,  FN8,  FN9,  NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   PWR,
+      NO,   NO,   NO,   UNDO, NO,   NO,   NO,   NO,   AGAIN, NO,  NO,   SLEP,
+      NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   WAKE,
+      TRNS, TRNS, TRNS, TRNS, FN4,  TRNS,       FN5,  MSTP, MPRV, MNXT, MPLY),
 };
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_BIT_XOR(0, 0b01, ON_BOTH), // to fn1/fn1+fn2 overlay
-    [2] = ACTION_LAYER_BIT_XOR(0, 0b10, ON_BOTH), // to fn2/fn1+fn2 overlay
-    [3] = ACTION_DEFAULT_LAYER_SET(0),  // set Qwerty layout
-    [4] = ACTION_DEFAULT_LAYER_SET(4),  // set Colemak layout
-    [5] = ACTION_DEFAULT_LAYER_SET(5),  // set Dvorak layout
+    [1] = ACTION_LAYER_BIT_XOR(1, 0b001, ON_BOTH), // fn1
+    [2] = ACTION_LAYER_BIT_XOR(1, 0b010, ON_BOTH), // fn2
+    [3] = ACTION_LAYER_BIT_OR( 1, 0b111, ON_BOTH), // fn1+fn2
+    [4] = ACTION_LAYER_BIT_XOR(1, 0b101, ON_BOTH), // fn1+fn2 -> fn2
+    [5] = ACTION_LAYER_BIT_XOR(1, 0b110, ON_BOTH), // fn1+fn2 -> fn1
+
+    [7] = ACTION_DEFAULT_LAYER_SET(0),  // set Qwerty layout
+    [8] = ACTION_DEFAULT_LAYER_SET(1),  // set Colemak layout
+    [9] = ACTION_DEFAULT_LAYER_SET(2),  // set Dvorak layout
+
     [10] = ACTION_MODS_KEY(MOD_LSFT, KC_1),
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_2),
     [12] = ACTION_MODS_KEY(MOD_LSFT, KC_3),
